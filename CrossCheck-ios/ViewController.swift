69d21e5c3a0bed9630fdddeb54377bbb8bd8d113
k3vkoh <kev33koh@gmail.com>
2022-04-28 14:23:10 -0400

    Initial Commit

---
+++ CrossCheck-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrossCheck-ios
+//
+//  Created by Chino on 4/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
