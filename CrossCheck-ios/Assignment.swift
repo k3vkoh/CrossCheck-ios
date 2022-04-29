7b6f9cbeb5a230403334b044a989dccb570331e4
k3vkoh <kev33koh@gmail.com>
2022-04-29 04:37:39 -0400

    assignments codable to parse api

---
+++ CrossCheck-ios/Assignment.swift
@@ -0,0 +1,19 @@
+//
+//  Assignment.swift
+//  CrossCheck-ios
+//
+//  Created by Chino on 4/29/22.
+//
+
+import Foundation
+
+struct Assignment: Codable {
+    var email: String
+    var course_num: String
+    var assignment_name: String
+    var due_date: String
+    var due_time: String
+    var day_name: String
+    var method: String
+    var submission_status: String
+}
